2b5c90f9a34b9e873ae23762f8034b53b1727f51
birth1984 <88521762@qq.com>
2019-02-28 16:00:11 +0800

    编辑器之Transform Inspector Editor
    在transform下面添加一个按钮

---
+++ Assets/Editor/RectTransformInspector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Transform))]
+public class RectTransformInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if(GUILayout.Button("Adding this button"))
+        {
+            Debug.Log("adfasdfasdf");
+        }
+    }
+}
