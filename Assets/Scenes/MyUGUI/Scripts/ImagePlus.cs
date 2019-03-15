2b79e37cd30b59d2614897884f632fa0bc357092
birth1984 <88521762@qq.com>
2019-03-15 19:02:34 +0800

    Create ImagePlus.cs

---
+++ Assets/Scenes/MyUGUI/Scripts/ImagePlus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//重写Image类，让Image不参与射线检测。
+public class ImagePlus : Image
+{
+    public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        return false; 
+
+        return base.IsRaycastLocationValid(screenPoint, eventCamera);
+    }
+}
