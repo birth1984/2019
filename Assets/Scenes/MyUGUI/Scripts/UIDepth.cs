ac0f425a01d4d792132b6ad63205d9669674676c
birth1984 <88521762@qq.com>
2019-03-15 19:03:08 +0800

    Create UIDepth.cs

---
+++ Assets/Scenes/MyUGUI/Scripts/UIDepth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDepth : MonoBehaviour
+{
+    public int order;
+    public bool isUI = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isUI)
+        {
+            Canvas canvas = GetComponent<Canvas>();
+            if(canvas == null )
+            {
+                canvas = gameObject.AddComponent<Canvas>();
+            }
+            canvas.overrideSorting = true;
+            canvas.sortingOrder = order;
+        }
+        else
+        {
+            Renderer[] renders = GetComponentsInChildren<Renderer>();
+            foreach (Renderer render in renders )
+            {
+                render.sortingOrder = order;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
