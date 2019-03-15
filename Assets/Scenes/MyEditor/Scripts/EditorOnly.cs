8d8659c6ba4f18721a354c8995f0fc7120038de5
birth1984 <88521762@qq.com>
2019-03-15 18:59:09 +0800

    Create EditorOnly.cs

---
+++ Assets/Scenes/MyEditor/Scripts/EditorOnly.cs
@@ -0,0 +1,74 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//Unity3D研究院编辑器之EditorOnly（二十八）
+//https://www.xuanyusong.com/archives/4384
+public class EditorOnly : MonoBehaviour
+{
+    [HideInInspector]
+    public string tag = "Untagged" ;
+
+    private void OnDrawGizmos()
+    {
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag(tag))
+        {
+            UnityEditor.Handles.Label(go.transform.position, tag);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[CustomEditor(typeof(EditorOnly))]
+public class EditorOnlyEditor:Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorOnly gizmos = target as EditorOnly;
+        EditorGUI.BeginChangeCheck();
+        gizmos.tag = EditorGUILayout.TagField("Tag for Object:", gizmos.tag);
+        if(EditorGUI.EndChangeCheck())
+        {
+            EditorUtility.SetDirty(gizmos);
+        }
+    }
+}
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
