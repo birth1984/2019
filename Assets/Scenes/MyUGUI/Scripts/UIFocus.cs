6971fd323216333bd4f4ca0746c3cb763ccb0346
birth1984 <88521762@qq.com>
2019-03-15 19:03:13 +0800

    Create UIFocus.cs

---
+++ Assets/Scenes/MyUGUI/Scripts/UIFocus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 如果你想整体的关闭某个父节点下的所有UI事件。把如下脚本绑定在父节点上即可。
+public class UIFocus : MonoBehaviour, ICanvasRaycastFilter
+{
+    public bool IsFocus = false;
+
+    public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+    {
+        return IsFocus;
+    }
+}
