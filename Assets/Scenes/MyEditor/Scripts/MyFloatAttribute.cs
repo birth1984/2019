da8a506950a48be3a4f81e94b8021f225abcf652
birth1984 <88521762@qq.com>
2019-04-11 11:14:58 +0800

    Create MyFloatAttribute.cs

---
+++ Assets/Scenes/MyEditor/Scripts/MyFloatAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFloatAttribute : PropertyAttribute
+{
+    public float floatMax;
+    public float floatMin;
+
+    public MyFloatAttribute(float max, float min)
+    {
+        floatMax = max;
+        floatMin = min;
+    }
+}
