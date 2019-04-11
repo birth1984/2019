dc0a2458bb2dfa46745bfdaa2ffd15efcd02dfb0
birth1984 <88521762@qq.com>
2019-04-11 11:17:27 +0800

    Create BtnAddListener.cs

---
+++ Assets/Scenes/MyUGUI/Scripts/BtnAddListener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnAddListener : MonoBehaviour
+{
+    [SerializeField]
+    private Button m_button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_button.onClick.AddListener(OnClickButton);
+    }
+
+    public void OnClickButton()
+    {
+        Debug.Log("Button is clicked");
+    }
+
+    
+}
