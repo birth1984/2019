21f9772546cc062babb866bde53913a776a62852
birth1984 <88521762@qq.com>
2019-04-11 11:16:26 +0800

    Create MyIntAttribute.cs

---
+++ Assets/Scenes/MyEditor/Scripts/MyIntAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyIntAttribute : PropertyAttribute
+{
+    public int intMax;
+    public int intMin;
+
+    public MyIntAttribute(int a , int b)
+    {
+        intMax = a;
+        intMin = b;
+    }
+
+    
+}
